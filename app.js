5247ba63c2d90255ea2accac22f6f49a8152dbdb
AbenezerKitata <abi4kit@gmail.com>
2022-01-25 23:58:27 -0800

    added a better function to do my operations

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
 const wrapper = document.getElementById("wrapper");
 const display = document.getElementById("display");
-const topDisplay = document.getElementById("top")
-const bottomDisplay = document.getElementById("bottom")
+const topDisplay = document.getElementById("top");
+const bottomDisplay = document.getElementById("bottom");
 const content = document.getElementById("content");
 const clear = document.querySelector(".clear");
-const btn0 = document.querySelector(".num0");
-const btn1 = document.querySelector(".num1");
-const btn2 = document.querySelector(".num2");
-const btn3 = document.querySelector(".num3");
-const btn4 = document.querySelector(".num4");
-const btn5 = document.querySelector(".num5");
-const btn6 = document.querySelector(".num6");
-const btn7 = document.querySelector(".num7");
-const btn8 = document.querySelector(".num8");
-const btn9 = document.querySelector(".num9");
 const square = document.querySelector(".square");
-const plus = document.querySelector(".plus");
-const minus = document.querySelector(".minus");
-const multiply = document.querySelector(".multiply");
-const divide = document.querySelector(".divide");
 const del = document.querySelector(".del");
 const percent = document.querySelector(".percent");
 const point = document.querySelector(".point");
@@ -26,92 +12,105 @@ const buttons = document.querySelectorAll(".button");
 const nums = document.querySelectorAll(".nums");
 const operators = document.querySelectorAll(".operators");
 const equal = document.querySelector(".equal");
-point.addEventListener("click",()=>{
-    bottomDisplay.innerText += "."
-})
-
+point.addEventListener("click", () => {
+  bottomDisplay.innerText += ".";
+});
 clear.addEventListener("click", clearText);
 equal.addEventListener("click", evaluate);
 square.addEventListener("click", squared);
-let operatorsArray = [...operators].map((operator)=>{
-    return operator.textContent
+del.addEventListener("click", () => {
+  let text = bottomDisplay.innerText;
+  let arr = [...text];
+  arr.pop();
+  let text2 = arr.toString().replace(/,/g, "");
+  bottomDisplay.innerText = text2;
 });
-
-
-del.addEventListener("click", ()=>{
-    let text = bottomDisplay.innerText
-    let arr = [...text];
-    arr.pop();
-    let text2 = arr.toString().replace(/,/g, "")
-    bottomDisplay.innerText = text2;
+nums.forEach((num) => {
+  num.addEventListener("click", displayText);
 });
-nums.forEach(num => {
-    num.addEventListener("click",displayText)
-});
-operators.forEach(operator=>{
-    operator.addEventListener("click",displayText)
+operators.forEach((operator) => {
+  operator.addEventListener("click", displayText);
 });
 function displayText(e) {
-    bottomDisplay.innerText += `${e.currentTarget.innerText}`;
-};
-function clearText(){
-    let text = bottomDisplay.innerText;
-    topDisplay.innerText = text;
-    bottomDisplay.innerText = null;
+  bottomDisplay.innerText += `${e.currentTarget.innerText}`;
 }
-function evaluate(){
-    let text = bottomDisplay.innerText;
-    let str = text.toString();
-    let arr = [...str];
-        if (arr.includes("+")){
-            let modArr = str.split("+");
-            if (modArr[0] === "") {
-                modArr.shift() 
-            }
-            topDisplay.innerText = text;
-            bottomDisplay.innerText = Number(modArr[0]) + Number(modArr[1]);
-        }
-        if (arr.includes("-")){
-            let modArr = str.split("-");
-            if (modArr[0] === "") {
-                modArr.shift() 
-            }
-            topDisplay.innerText = text;
-            bottomDisplay.innerText = Number(modArr[0]) - Number(modArr[1]);
-        }
-        if (arr.includes("X")){
-            let modArr = str.split("X");
-            if (modArr[0] === "") {
-                modArr.shift() 
-            }
-            topDisplay.innerText = text;
-            bottomDisplay.innerText = Number(modArr[0]) * Number(modArr[1])
-        }
-        if (arr.includes("/")){
-            let modArr = str.split("/");
-            if (modArr[0] === "") {
-                modArr.shift() 
-            }
-            topDisplay.innerText = text;
-            bottomDisplay.innerText = Number(modArr[0]) / Number(modArr[1])
-        }
-        if (arr.includes("%")){
-            let modArr = str.split("%");
-            if (modArr[0] === "") {
-                modArr.shift() 
-            }
-            topDisplay.innerText = text;
-            bottomDisplay.innerText = Number(modArr[0]) % Number(modArr[1])
-        }
-};
-function squared(){
-    let text = bottomDisplay.innerText;
-    topDisplay.innerText = text;
-    bottomDisplay.innerText = text**2;
+function clearText() {
+  let text = bottomDisplay.innerText;
+  topDisplay.innerText = text;
+  bottomDisplay.innerText = ``;
 }
-// function fraction(){
-//     let text = bottomDisplay.innerText;
-//     topDisplay.innerText = text;
-//     bottomDisplay.innerText = Number(`1/${text}`);
-// }
 
+function squared() {
+  let text = bottomDisplay.innerText;
+  topDisplay.innerText = text;
+  bottomDisplay.innerText = text ** 2;
+}
+function evaluate() {
+  let text = bottomDisplay.innerText;
+  let str = text.toString();
+  let arr = [...str];
+  console.log(arr);
+  console.log(str);
+  let splited = arr.map((itm) => {
+    if (arr.includes("%")) {
+      let modArr = str.split("%");
+      topDisplay.innerText = text;
+      if (Number(modArr[0]) % Number(modArr[1] === NaN)) {
+        bottomDisplay.innerText = `Syntax Error!`;
+      }
+      bottomDisplay.innerText = Number(modArr[0]) % Number(modArr[1]);
+    }
+    if (itm === "+") {
+      let ans = str.split(itm);
+      let added = ans.reduce(function (acc, ar) {
+        return Number(acc) + Number(ar);
+      }, 0);
+      topDisplay.innerText = text;
+      if (added === NaN) {
+        bottomDisplay.innerText = `Syntax Error!`;
+      }
+      bottomDisplay.innerText = added;
+    }
+    if (itm === "-") {
+      let ans = str.split(itm);
+      let sub = ans.reduce(function (acc, ar) {
+        return Number(acc) - Number(ar);
+      }, 0);
+      topDisplay.innerText = text;
+      if (sub === NaN) {
+        bottomDisplay.innerText = `Syntax Error!`;
+      }
+      bottomDisplay.innerText = sub;
+    }
+    if (itm === "X") {
+      let ans = str.split(itm);
+      let multiply = ans.reduce(function (acc, ar) {
+        return Number(acc) * Number(ar);
+      });
+      console.log(multiply);
+      topDisplay.innerText = text;
+      if (multiply === NaN) {
+        bottomDisplay.innerText = `Syntax Error!`;
+      }
+      bottomDisplay.innerText = multiply;
+    }
+    if (itm === "/") {
+      let ans = str.split(itm);
+      let divide = ans.reduce(function (acc, ar) {
+        return Number(acc) / Number(ar);
+      });
+      topDisplay.innerText = text;
+      if (divide === NaN) {
+        bottomDisplay.innerText = `Syntax Error!`;
+      }
+      bottomDisplay.innerText = divide;
+    }
+  });
+
+  return splited;
+};
+bottomDisplay.addEventListener('keydown', (e)=>{
+    if (e.key === 1) {
+        
+    }
+})
